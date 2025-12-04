8e1b9b686aaf59725271db28203fdc3868b8902e
cavins01 <cavina326@gmail.com>
2025-12-04 19:30:37 +0100

    Aggiunta .gitignore

--- recensioni-seguite/src/main/java/asw/bettermusic/recensioniseguite/eventsconsumer/AlbumEventsConsumer.java
+++ recensioni-seguite/src/main/java/asw/bettermusic/recensioniseguite/eventsconsumer/AlbumEventsConsumer.java
@@ -2,18 +2,20 @@ package asw.bettermusic.recensioniseguite.eventsconsumer;
 
 import asw.bettermusic.album.api.events.AlbumCreatedEvent;
 import asw.bettermusic.recensioniseguite.domain.*;
-import org.springframework.stereotype.Service;
+
+import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import asw.bettermusic.album.api.events.AlbumServiceEventChannel;
 
-@Service
+@Component
 public class AlbumEventsConsumer {
 
   @Autowired
   private RecensioniSeguiteEventListener recensioniSeguiteEventListener;
 
-  @KafkaListener(topics = AlbumServiceEventChannel.channel, groupId = "recensioni-seguite")
+  @KafkaListener(topics = AlbumServiceEventChannel.channel, groupId = "recensioni-seguite",
+    properties = "spring.json.value.default.type=asw.bettermusic.album.api.events.AlbumCreatedEvent")
   public void listen(AlbumCreatedEvent event) {
     recensioniSeguiteEventListener.onAlbumCreated(event);
   }
